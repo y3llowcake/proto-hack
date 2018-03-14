21a32477a4ae193b5249549893569a47b656cf39
Cyrus Katrak <ckatrak@slack-corp.com>
2018-03-14 13:31:42 -0700

    holy moly this works

--- test/example1.php
+++ test/example1.php
@@ -5,15 +5,35 @@ include "./gen-src/foo/proto_types.php";
 
 function a(mixed $got, mixed $exp, string $msg) {
 	if ($got != $exp) {
-		throw new Exception($msg . "; got:\n" . print_r($got, true) . "\n expected:\n" . print_r($exp, true));
+		throw new Exception($msg . "; got:\n" . print_r($got, true) . "\n expected:\n" . print_r($exp, true) . "\ndiff:\n" . diff($got, $exp));
 	}
+}
+
+function diff(mixed $got, mixed $exp): string {
+	if (!is_object($got) || !is_object($exp) || get_class($got) != get_class($exp)) {
+		return "<not diffable>";
+	}
+	$rexp = new ReflectionClass($exp);
+	$rgot = new ReflectionClass($got);
+	foreach ($rexp->getProperties() as $prop) {
+		$gotval = $prop->getValue($got);
+		$expval = $rexp->getProperty($prop->name)->getValue($exp);
+		if ($gotval != $expval) {
+			return "property: {$prop->name} got: $gotval expected: $expval";
+		}
+	}
+	return "<empty diff>";
+}
+
+function repackFloat(float $f): float {
+	return unpack("f", pack("f", $f))[1];
 }
 
 $got = new foo\example1();
 Protobuf\Internal\Unmarshal(file_get_contents('./gen-data/example1.pb.bin'), $got);
 $exp = new foo\example1();
 $exp->adouble = 13.37;
-$exp->afloat = 20.10000038147;
+$exp->afloat = repackFloat(100.1);
 $exp->aint32 = 1;
 $exp->aint64 = 12;
 $exp->auint32 = 123;
@@ -27,6 +47,9 @@ $exp->asfixed64 = 1234567890;
 $exp->abool = true;
 $exp->astring = "foobar";
 $exp->abytes = "hello world";
+
+echo "derp " . ($exp->afloat - $got->afloat)  .  "\n";
+
 //print_r($got);
 a($got, $exp, "example1 mismatch");
 
