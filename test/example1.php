2996366883f0c068287c6f894f4ea3983440bf34
Cyrus Katrak <ckatrak@slack-corp.com>
2018-03-14 11:29:10 -0700

    get the tests passing again

--- test/example1.php
+++ test/example1.php
@@ -2,7 +2,7 @@
 include "../lib/protobuf.php";
 include "./gen-src/foo/proto_types.php";
 
-$data = file_get_contents("./gen-data/example.pb.bin");
-$e = new foo\ExampleOne();
+$data = file_get_contents("./gen-data/example1.pb.bin");
+$e = new foo\example1();
 Protobuf\Internal\Unmarshal($data, $e);
 print_r($e);
