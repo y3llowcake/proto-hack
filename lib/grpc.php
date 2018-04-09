df704988dc1d1612e72a69f7fe0c8a6c3a40a7f8
Cyrus Katrak <ckatrak@slack-corp.com>
2018-04-09 13:40:43 -0700

    hh_format

--- lib/grpc.php
+++ lib/grpc.php
@@ -1,36 +1,39 @@
 <?hh // strict
 
 namespace Grpc {
-	newtype Code = int;
-	abstract class Codes {
-		const Code OK = 0;
-		const Code Canceled = 1;
-		const Code Unknown = 2;
-		const Code InvalidArgument = 3;
-		const Code DeadlineExceeded = 4;
-		const Code NotFound = 5;
-		const Code AlreadyExists = 6;
-		const Code PermissionDenied = 7;
-		const Code ResourceExhausted = 8;
-		const Code FailedPrecondition = 9;
-		const Code Aborted = 10;
-		const Code OutOfRange = 11;
-		const Code Unimplemented = 12;
-		const Code Internal = 13;
-		const Code Unavailable = 14;
-		const Code DataLoss = 15;
-		const Code Unauthenticated = 16;
-	}
+  newtype Code = int;
+  abstract class Codes {
+    const Code OK = 0;
+    const Code Canceled = 1;
+    const Code Unknown = 2;
+    const Code InvalidArgument = 3;
+    const Code DeadlineExceeded = 4;
+    const Code NotFound = 5;
+    const Code AlreadyExists = 6;
+    const Code PermissionDenied = 7;
+    const Code ResourceExhausted = 8;
+    const Code FailedPrecondition = 9;
+    const Code Aborted = 10;
+    const Code OutOfRange = 11;
+    const Code Unimplemented = 12;
+    const Code Internal = 13;
+    const Code Unavailable = 14;
+    const Code DataLoss = 15;
+    const Code Unauthenticated = 16;
+  }
 
-	class GrpcException extends \Exception {
-		public Code $grpc_code;
-		public string $grpc_message;
-		public function __construct(Code $code, string $message){
-			parent::__construct(sprintf("grpc exception: %d; %s", $code, $message), $code);
-			$this->grpc_code = $code;
-			$this->grpc_message = $message;
-		}
-	}
+  class GrpcException extends \Exception {
+    public Code $grpc_code;
+    public string $grpc_message;
+    public function __construct(Code $code, string $message) {
+      parent::__construct(
+        sprintf("grpc exception: %d; %s", $code, $message),
+        $code,
+      );
+      $this->grpc_code = $code;
+      $this->grpc_message = $message;
+    }
+  }
 
   use \Protobuf\Message;
 
@@ -50,7 +53,11 @@ namespace Grpc {
 
   interface ServerDispatch {
     public function Name(): string;
-    public function Dispatch(Context $ctx, string $method, string $rawin): string;
+    public function Dispatch(
+      Context $ctx,
+      string $method,
+      string $rawin,
+    ): string;
   }
 
 }
