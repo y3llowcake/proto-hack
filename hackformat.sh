5ab6da3598963bf4af04aa22de21a530f9dd68dd
Cyrus Katrak <ckatrak@slack-corp.com>
2019-07-01 13:31:51 -0700

    rename example1.php test.php

--- hackformat.sh
+++ hackformat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-for i in `find lib | grep \.php | grep -v \.swp | grep -v 'wellknowntype/.*_proto.php'`
+FILES=`find lib | grep \.php | grep -v \.swp | grep -v 'wellknowntype/.*_proto.php'`
+FILES="$FILES test/test.php"
+for i in $FILES
 do
   echo formatting $i
   hackfmt -i $i
