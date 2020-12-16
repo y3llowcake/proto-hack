a9328678ca32a261d413f6d5dbe441655cc3b4eb
Cyrus Katrak <ckatrak@slack-corp.com>
2020-12-16 08:39:38 -0800

    fix integration tests

--- hackformat.sh
+++ hackformat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FILES=`find lib | grep \.php | grep -v \.swp | grep -v 'wellknowntype/.*_proto.php'`
-FILES="$FILES test/test.php"
+FILES="$FILES test/test.php test/test_suite.php"
 FILES="$FILES conformance/conformance.php"
 for i in $FILES
 do
